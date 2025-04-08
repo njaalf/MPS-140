95fc00cd007167feef8bd85975cec30f643ba1e7
Njål Foldnes <njal.foldnes@bi.no>
2025-04-08 14:08:58 +0200

    ryddet

--- MPS-140/README.md
+++
@@ -1,1 +0,0 @@
-Just a website for students of MPS140 Statistics at the University of Stavanger.
--- MPS-140/eksamen/fasit.R
+++
@@ -1,6 +0,0 @@
-mydata <- readRDS("datafil.rds")
-table(complete.cases(mydata))
-
-mydata <- mydata[complete.cases(mydata),  ]
-
-summary(mydata$Alder)
